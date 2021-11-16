88fec71e1bc0638b8b806a08411e08bb86e95bf6
Bastian <bastian.bjorn@gmail.com>
2021-11-16 21:35:33 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,50 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <h1>My Blogg</h1>
+      <div className="navigation-bar">
+        <p>Home</p>
+        <p>Blog Posts</p>
+        <p>About</p>
+        <p>Contact</p>
+      </div>
+      <div className="posts">
+        <p className="">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+          placeat facilis nulla, doloribus neque, repudiandae repellat
+          cupiditate earum a aut id beatae, optio voluptatibus pariatur aperiam
+          tenetur iste ex temporibus? Excepturi architecto vel magni earum
+          pariatur sint doloremque quasi consectetur dolorem dolore voluptatum
+          quod obcaecati, enim distinctio quidem illum nostrum!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+          placeat facilis nulla, doloribus neque, repudiandae repellat
+          cupiditate earum a aut id beatae, optio voluptatibus pariatur aperiam
+          tenetur iste ex temporibus? Excepturi architecto vel magni earum
+          pariatur sint doloremque quasi consectetur dolorem dolore voluptatum
+          quod obcaecati, enim distinctio quidem illum nostrum!
+        </p>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+          placeat facilis nulla, doloribus neque, repudiandae repellat
+          cupiditate earum a aut id beatae, optio voluptatibus pariatur aperiam
+          tenetur iste ex temporibus? Excepturi architecto vel magni earum
+          pariatur sint doloremque quasi consectetur dolorem dolore voluptatum
+          quod obcaecati, enim distinctio quidem illum nostrum!
+        </p>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+          placeat facilis nulla, doloribus neque, repudiandae repellat
+          cupiditate earum a aut id beatae, optio voluptatibus pariatur aperiam
+          tenetur iste ex temporibus? Excepturi architecto vel magni earum
+          pariatur sint doloremque quasi consectetur dolorem dolore voluptatum
+          quod obcaecati, enim distinctio quidem illum nostrum!
+        </p>
+      </div>
     </div>
   );
 }
